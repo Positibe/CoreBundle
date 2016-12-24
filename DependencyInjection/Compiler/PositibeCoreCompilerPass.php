530dd67a824a8019368ba878a6af165e67bbdf34
Pedro Carlos Abreu <pcabreus@gmail.com>
2016-12-23 20:09:13 -0500

    Some files forgetted

---
+++ DependencyInjection/Compiler/PositibeCoreCompilerPass.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the PositibeLabs Projects.
+ *
+ * (c) Pedro Carlos Abreu <pcabreus@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Positibe\Bundle\CoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+
+/**
+ * Class PositibeCoreCompilerPass
+ * @package Positibe\Bundle\OrmContentBundle\DependencyInjection\Compiler
+ *
+ * @author Pedro Carlos Abreu <pcabreus@gmail.com>
+ */
+class PositibeCoreCompilerPass implements CompilerPassInterface
+{
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $sluggableListener = $container->getDefinition('stof_doctrine_extensions.listener.sluggable');
+        $sluggableListener->addMethodCall('addManagedFilter', array('softdeleteable'));
+
+        $container->setParameter(
+          'lunetics_locale.request_listener.class',
+          'Positibe\Bundle\CoreBundle\EventListener\LocaleMatcherListener'
+        );
+    }
+
+} 
---
+++ DependencyInjection/PositibeCoreExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Positibe\Bundle\CoreBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class PositibeCoreExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $bundles = $container->getParameter('kernel.bundles');
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        if (isset($bundles['CmfSeoBundle'])) {
+            $loader->load('cmf_seo_extractor_services.yml');
+        }
+
+        $loader->load('twig_extension_services.yml');
+        $loader->load('services.yml');
+
+        if(isset($bundles['LuneticsLocalebundle']))
+        {
+            $this->addClassesToCompile(array(
+                'Lunetics\\LocaleBundle\\EventListener\\LocaleListener',
+                'Lunetics\\LocaleBundle\\LocaleGuesser\\LocaleGuesserManager',
+                'Lunetics\\LocaleBundle\\Matcher\\DefaultBestLocaleMatcher',
+                'Lunetics\\LocaleBundle\\EventListener\\LocaleUpdateListener',
+                'Lunetics\\LocaleBundle\\Cookie\\LocaleCookie',
+                'Lunetics\\LocaleBundle\\Session\\LocaleSession',
+                'Positibe\\Bundle\\CoreBundle\\EventListener\\LocaleMatcherListener',
+                'Stof\\DoctrineExtensionsBundle\\EventListener\\LocaleListener',
+                'Positibe\\Bundle\\CoreBundle\\EventListener\\LocaleListener',
+                'Symfony\\Cmf\\Bundle\\CoreBundle\\EventListener\\PublishWorkflowListener',
+                'Symfony\Cmf\Bundle\SeoBundle\EventListener\ContentListener'
+              ));
+        }
+    }
+}
