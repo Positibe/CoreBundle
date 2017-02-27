1863364b7930be7cd688d33e3c189653bf3c11d8
Pedro Carlos Abreu <pcabreus@gmail.com>
2017-02-26 19:32:05 -0500

    added some theme support for mdbootstrap

---
+++ Form/Extension/FormTypeExtension.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of the PositibeLabs Projects.
+ *
+ * (c) Pedro Carlos Abreu <pcabreus@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Positibe\Bundle\CoreBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * This class provide the help option to all form's type
+ *
+ * Class FormTypeExtension
+ * @package Positibe\Bundle\CoreBundle\Form\Extension
+ *
+ * @author Pedro Carlos Abreu <pcabreus@gmail.com>
+ */
+class FormTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return FormType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults(['help' => null]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        $view->vars['help'] = $options['help'];
+    }
+
+}
