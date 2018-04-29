2fc9bfb844e04683c3a3bf387a900c0a49a7d2b0
Pedro Carlos Abreu <pcabreus@gmail.com>
2018-04-29 11:34:42 +0000

    1.0.3 released

--- Twig/Extension/PositibeCoreExtension.php
+++ Twig/Extension/PositibeCoreExtension.php
@@ -7,9 +7,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that is distributed with this source code.
  */
+
 namespace Positibe\Bundle\CoreBundle\Twig\Extension;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class LocaleSwitcherExtension
@@ -48,7 +50,47 @@ class PositibeCoreExtension extends \Twig_Extension
                 [$this, 'renderSortingLink'],
                 ['needs_environment' => true, 'is_safe' => ['html']]
             ),
+            new \Twig_SimpleFunction(
+                'remove_filter',
+                [$this, 'removeFilter'],
+                ['is_safe' => ['html']]
+            ),
         ];
+    }
+
+    public function removeFilter(Request $request, $filter)
+    {
+        //Base on of Request->getUri()
+        $query = $request->server->get('QUERY_STRING');
+        $parts = [];
+        $order = [];
+
+        foreach (explode('&', $query) as $param) {
+            if ('' === $param || '=' === $param[0]) {
+                // Ignore useless delimiters, e.g. "x=y&".
+                // Also ignore pairs with empty key, even if there was a value, e.g. "=value", as such nameless values cannot be retrieved anyway.
+                // PHP also does not include them when building _GET.
+                continue;
+            }
+
+            $keyValuePair = explode('=', $param, 2);
+
+            // GET parameters, that are submitted from a HTML form, encode spaces as "+" by default (as defined in enctype application/x-www-form-urlencoded).
+            // PHP also converts "+" to spaces when filling the global _GET or when using the function parse_str. This is why we use urldecode and then normalize to
+            // RFC 3986 with rawurlencode.
+            if (urldecode($keyValuePair[0]) === $filter) {
+                // Elimina el parametro
+                $parts[] = rawurlencode(urldecode($keyValuePair[0]));
+            } else {
+                $parts[] = isset($keyValuePair[1]) ?
+                    rawurlencode(urldecode($keyValuePair[0])).'='.rawurlencode(urldecode($keyValuePair[1])) :
+                    rawurlencode(urldecode($keyValuePair[0]));
+            }
+            $order[] = urldecode($keyValuePair[0]);
+            array_multisort($order, SORT_ASC, $parts);
+        }
+
+        return $request->getSchemeAndHttpHost().$request->getBaseUrl().$request->getPathInfo().'?'.implode('&', $parts);
     }
 
     /**
@@ -62,6 +104,8 @@ class PositibeCoreExtension extends \Twig_Extension
     }
 
     /**
+     * @deprecated use onclick="history.back();" on a HTML element instead
+     *
      * @param $route
      * @param array $parameters
      * @return mixed|string
